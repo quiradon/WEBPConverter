123d43782fbf6db7694dc846a2a8b77c29afbf25
quir <quir@arkanus.app>
2024-08-22 01:22:46 -0300

    add Multi tread

--- index.py
+++ index.py
@@ -2,19 +2,31 @@
 
 import os
 from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
 
 # Configurações
 input_path = 'input'
 output_path = 'out'
 image_quality = 75
+max_workers = 16  # Número de threads
 
-for file in os.listdir(input_path):
+def process_image(file):
     if file.endswith('.jpg') or file.endswith('.png'):
-        # Abre a imagem
-        image = Image.open(f'{input_path}/{file}')
-        # Salva a imagem em webp
-        image.save(f'{output_path}/{file.rsplit(".", 1)[0]}.webp', 'webp', quality=image_quality)
-        print(f'{file} convertido com sucesso')
+        try:
+            # Abre a imagem
+            image = Image.open(f'{input_path}/{file}')
+            # Salva a imagem em webp
+            image.save(f'{output_path}/{file.rsplit(".", 1)[0]}.webp', 'webp', quality=image_quality)
+            print(f'{file} convertido com sucesso')
+        except Exception as e:
+            print(f'Erro ao converter {file}: {e}')
 
-print('Conversão finalizada')
+def main():
+    files = os.listdir(input_path)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        executor.map(process_image, files)
+
+if __name__ == '__main__':
+    main()
+    print('Conversão finalizada')
 
